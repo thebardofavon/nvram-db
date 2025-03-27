77880be7b6ebcbd4be669c01caf32029f2db8091
Anushka <152669825+thebardofavon@users.noreply.github.com>
2025-03-27 14:22:07 +0530

    Comment out RAM free space management function declarations in ram_free_space.h

--- src_db_v2/src/ram_free_space.h
+++ src_db_v2/src/ram_free_space.h
@@ -1,21 +1,21 @@
-#ifndef RAM_FREE_SPACE_H
-#define RAM_FREE_SPACE_H
+// #ifndef RAM_FREE_SPACE_H
+// #define RAM_FREE_SPACE_H
 
-#include <stddef.h>
+// #include <stddef.h>
 
-// Initialize RAM free space management system
-void ram_init_free_space();
+// // Initialize RAM free space management system
+// void ram_init_free_space();
 
-// Allocate memory from RAM using first-fit
-void *ram_allocate_memory(size_t size);
+// // Allocate memory from RAM using first-fit
+// void *ram_allocate_memory(size_t size);
 
-// Free allocated RAM memory and merge adjacent blocks
-void ram_free_memory(void *ptr, size_t size);
+// // Free allocated RAM memory and merge adjacent blocks
+// void ram_free_memory(void *ptr, size_t size);
 
-// Get current RAM free space statistics
-void ram_get_stats(size_t *total_free, size_t *largest_block, int *free_blocks);
+// // Get current RAM free space statistics
+// void ram_get_stats(size_t *total_free, size_t *largest_block, int *free_blocks);
 
-// Cleanup function to release RAM resources
-void ram_cleanup_free_space();
+// // Cleanup function to release RAM resources
+// void ram_cleanup_free_space();
 
-#endif // RAM_FREE_SPACE_H
+// #endif // RAM_FREE_SPACE_H
